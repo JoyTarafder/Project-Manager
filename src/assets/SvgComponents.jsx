9f751b66de4552bce12645c83152a4fac856fd20
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2024-10-26 14:17:05 +0600

    react tostly added

---
+++ src/assets/SvgComponents.jsx
@@ -0,0 +1,81 @@
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+export function EditSVG() {
+  const Working = () => toast.warning("Edit button not available !");
+  return (
+    <>
+      <svg
+        onClick={Working}
+        className="h-4 w-4 cursor-pointer text-zinc-300"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+        ></path>
+      </svg>
+      <ToastContainer />
+    </>
+  );
+}
+export function DeleteSVG() {
+  const Working = () => toast.warning("Delete button not available !");
+  return (
+    <>
+      <svg
+        onClick={Working}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="h-4 w-4 cursor-pointer text-zinc-300"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M4 7l16 0" />
+        <path d="M10 11l0 6" />
+        <path d="M14 11l0 6" />
+        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+      </svg>
+      <ToastContainer />
+    </>
+  );
+}
+export default function FilterSVG() {
+  const Working = () => toast.warning("Filter button not available !");
+  return (
+    <>
+      <svg
+        onClick={Working}
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="18"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-sort-descending"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M4 6l9 0" />
+        <path d="M4 12l7 0" />
+        <path d="M4 18l7 0" />
+        <path d="M15 15l3 3l3 -3" />
+        <path d="M18 6l0 12" />
+      </svg>
+      <ToastContainer />
+    </>
+  );
+}
